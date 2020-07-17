5ad9e026c1de8ca2c1ff21fa85bad827712bf0cc
Minerva Archer <minerva.archer@gmail.com>
2020-07-17 13:28:49 -0400

    add icons to manifest config

--- webpack.config.js
+++ webpack.config.js
@@ -48,301 +48,347 @@ const buildStyleLoaders = ({ modules = false, mode = 'production'}) => {
 };
 
 module.exports = (env, argv) => ({
-    mode: argv.mode || 'production',
-    context: path.resolve(__dirname),
-    devtool: argv.mode === 'production' ? 'source-map' : 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        hot: argv.mode !== 'production',
-        historyApiFallback: true,
-        host: 'localhost.minervaarcher',
-        port: 3000,
+  mode: argv.mode || "production",
+  context: path.resolve(__dirname),
+  devtool:
+    argv.mode === "production" ? "source-map" : "cheap-module-eval-source-map",
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+    hot: argv.mode !== "production",
+    historyApiFallback: true,
+    host: "localhost.minervaarcher",
+    port: 3000,
+  },
+  entry: {
+    app: "./src/main.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename:
+      argv.mode === "production" ? "js/[name].[chunkhash:8].js" : "[name].js",
+    publicPath: "/",
+    chunkFilename:
+      argv.mode === "production" ? "js/[name].[chunkhash:8].js" : "[name].js",
+  },
+  resolve: {
+    symlinks: false,
+    alias: {
+      "@": path.resolve(__dirname, "src"),
     },
-    entry: {
-        app: './src/main.js'
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: argv.mode === 'production' ? 'js/[name].[chunkhash:8].js' : '[name].js',
-        publicPath: '/',
-        chunkFilename: argv.mode === 'production' ? 'js/[name].[chunkhash:8].js' : '[name].js',
-    },
-    resolve: {
-        symlinks: false,
-        alias: {
-            '@': path.resolve(__dirname, 'src'),
-        },
-        extensions: [
-            '.js',
-            '.vue',
-            '.json',
-        ],
-        modules: [
-            'node_modules',
-            path.resolve(__dirname, 'node_modules'),
-        ],
-    },
-    resolveLoader: {
-        modules: [
-            'node_modules',
-            path.resolve(__dirname, 'node_modules'),
-        ],
-    },
-    ...(argv.mode === 'production' ?
-        { 
-            optimization: {
-                minimize: true,
-                minimizer: [
-                    new TerserPlugin({
-                        test: /\.js(\?.*)?$/i,
-                        include: path.resolve(__dirname, 'src'),
-                        cache: true,
-                        terserOptions: {
-                            output: {
-                                comments: /^\**!|@preserve|@license|@cc_on/
-                            },
-                            mangle: {
-                                safari10: true
-                            }
-                        },
-                    })
-                ],
-                splitChunks: {
-                    cacheGroups: {
-                        vendors: {
-                            name: 'chunk-vendors',
-                            test: /[\\/]node_modules[\\/]/,
-                            priority: -10,
-                            chunks: 'initial',
-                        },
-                        common: {
-                            name: 'chunk-common',
-                            minChunks: 2,
-                            priority: -20,
-                            chunks: 'initial',
-                            reuseExistingChunk: true,
-                        }
-                    }
-                }
-            } 
-        } :
-        {}
-    ),
-    module: {
-        noParse: /^(vue|vue-router|vuex)$/,
-        rules: [
-            {
-                test: /\.vue$/i,
-                use: [
-                    {
-                        loader: 'vue-loader',
-                        options: {
-                            compilerOptions: {
-                                preserveWhiteSpace: argv.mode !== 'production',
-                            }
-                        }
-                    }
-                ],
-                exclude: path.resolve(__dirname, 'node_modules'),
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                oneOf: [
-                    {
-                        resourceQuery: /module/,
-                        use: buildStyleLoaders({ modules: true, mode: argv.mode }),
-                    },
-                    {
-                        resourceQuery:  /\?vue/,
-                        use: buildStyleLoaders({ mode: argv.mode }),
-                    },
-                    {
-                        test: /\.module\.\w+$/,
-                        use: buildStyleLoaders({ modules: true, mode: argv.mode }),
-                    },
-                    {
-                        use: buildStyleLoaders({ mode: argv.mode }),
-                    },
-                ],
-                exclude: path.resolve(__dirname, 'node_modules'),
-            },
-            {
-                test:/\.(png|jpe?g|gif|webp)(\?.*)?$/,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            limit: 4096,
-                            name: 'img/[name].[hash:8].[ext]',
-                        }
-                    }
-                ],
-                exclude: path.resolve(__dirname, 'node_modules'),
-            },
-            {
-                test: /\.(svg)(\?.*)?$/,
-                loader: 'file-loader',
-                options: {
-                    name: 'img/[name].[hash:8].[ext]',
+    extensions: [".js", ".vue", ".json"],
+    modules: ["node_modules", path.resolve(__dirname, "node_modules")],
+  },
+  resolveLoader: {
+    modules: ["node_modules", path.resolve(__dirname, "node_modules")],
+  },
+  ...(argv.mode === "production"
+    ? {
+        optimization: {
+          minimize: true,
+          minimizer: [
+            new TerserPlugin({
+              test: /\.js(\?.*)?$/i,
+              include: path.resolve(__dirname, "src"),
+              cache: true,
+              terserOptions: {
+                output: {
+                  comments: /^\**!|@preserve|@license|@cc_on/,
                 },
-                exclude: path.resolve(__dirname, 'node_modules'),
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use:  [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            limit: 10000,
-                            name: 'fonts/[name].[hash:7].[ext]'
-                        }
-                    }
-                ],
-                include: path.resolve(__dirname, 'src/fonts'),
-            },
-            {
-                enforce: 'pre',
-                test: /\.(vue|js)$/,
-                exclude: [
-                    /node_modules/
-                ],
-                use: [
-                    {
-                        loader: 'eslint-loader',
-                        options: {
-                            extensions: [
-                                '.js',
-                                '.vue',
-                            ],
-                            emitWarning: true,
-                            formatter: 'stylish',
-                        }
-                    }
-                ]
-            },
-        ]
-    },
-    plugins: [
-        new VueLoaderPlugin(),
-        new webpack.DefinePlugin(
-            {
-              BASE_URL: '"/"',
-              'process.env': {
-                BASE_URL: '"/"'
-              }
-            }
-        ),
-        new CaseSensitivePathsPlugin(),
-        new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
-        ...(argv.mode === 'production' ? 
-            [
-                new MiniCssExtractPlugin({
-                    filename: 'css/[name].[contenthash:8].css',
-                    chunkFilename: 'css/[name].[contenthash:8].css',
-                }),
-                new OptimizeCssAssetsPlugin({
-                    canPrint: false,
-                    cssProcessorOptions: {
-                        safe: true,
-                        autoprefixer: {
-                            disable: true
-                        },
-                        mergeLonghand: false
-                    }
-                }),
-            ] : 
-            [
-                new webpack.HotModuleReplacementPlugin(),
-                new webpack.NoEmitOnErrorsPlugin(),
-                new webpack.ProgressPlugin(),
-            ]
-        ),
-        new HtmlWebpackPlugin({
-            templateParameters: (compilation, assets, pluginOptions) => {
-                // enhance html-webpack-plugin's built in template params
-                let stats
-                return {
-                  // make stats lazy as it is expensive
-                  get webpack () {
-                    return stats || (stats = compilation.getStats().toJson())
-                  },
-                  compilation: compilation,
-                  webpackConfig: compilation.options,
-                  htmlWebpackPlugin: {
-                    files: assets,
-                    options: pluginOptions
-                  }
-                };
+                mangle: {
+                  safari10: true,
+                },
               },
-            ...(argv.mode === 'production' ?
-                {
-                    minify: {
-                        removeComments: true,
-                        collapseWhitespace: true,
-                        removeAttributeQuotes: true,
-                        collapseBooleanAttributes: true,
-                        removeScriptTypeAttributes: true,
-                    },
-                } : 
-                {}),
-            template: path.resolve(__dirname, 'public/index.html'),
-        }),
-        new HtmlPwaPlugin({
-            name: 'Minerva Archer',
-            short_name: 'Minerva',
-            themeColor: '#000000',
-            msTileColor: '#FFFFFF',
-            manifestOptions: {
-                name: 'Minerva Archer',
-                short_name: 'Minerva',
-                description: 'The personal website of Minerva Archer.',
-                developer: {
-                    name: 'Minerva Archer',
-                    url: 'https://minervaarcher.com'
-                },
-                start_url: '/index.html',
-                theme_color: '#000000',
-                background_color: '#FFFFFF',
-            }
-        }),
-        new PreloadPlugin({
-            rel: 'preload',
-            include: 'initial',
-            fileBlacklist: [
-                /\.map$/,
-                /hot-update\.js$/
-            ]
-        }),
-        new PreloadPlugin({
-            rel: 'prefetch',
-            include: 'asyncChunks'
-        }),
-        new CopyPlugin({
-            patterns: [
-                {
-                    from: path.resolve(__dirname, 'public'),
-                    to: path.resolve(__dirname, 'dist'),
-                    toType: 'dir',
-                    globOptions: {
-                        ignore: [
-                            '**/index.html',
-                            '**/.DS_Store'
-                        ],
-                    }
-                }
-            ]
-        }),
-        ...(argv.mode === 'production' ? [
-            new GenerateSW({
-                cleanupOutdatedCaches: true,
-                exclude: [
-                    /\.map$/,
-                    /img\/icons\//,
-                    /favicon\.ico$/,
-                    /manifest\.json$/
-                ],
-                cacheId: 'minervaarcher-vue'
             }),
-        ] : [])
-    ]
+          ],
+          splitChunks: {
+            cacheGroups: {
+              vendors: {
+                name: "chunk-vendors",
+                test: /[\\/]node_modules[\\/]/,
+                priority: -10,
+                chunks: "initial",
+              },
+              common: {
+                name: "chunk-common",
+                minChunks: 2,
+                priority: -20,
+                chunks: "initial",
+                reuseExistingChunk: true,
+              },
+            },
+          },
+        },
+      }
+    : {}),
+  module: {
+    noParse: /^(vue|vue-router|vuex)$/,
+    rules: [
+      {
+        test: /\.vue$/i,
+        use: [
+          {
+            loader: "vue-loader",
+            options: {
+              compilerOptions: {
+                preserveWhiteSpace: argv.mode !== "production",
+              },
+            },
+          },
+        ],
+        exclude: path.resolve(__dirname, "node_modules"),
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        oneOf: [
+          {
+            resourceQuery: /module/,
+            use: buildStyleLoaders({ modules: true, mode: argv.mode }),
+          },
+          {
+            resourceQuery: /\?vue/,
+            use: buildStyleLoaders({ mode: argv.mode }),
+          },
+          {
+            test: /\.module\.\w+$/,
+            use: buildStyleLoaders({ modules: true, mode: argv.mode }),
+          },
+          {
+            use: buildStyleLoaders({ mode: argv.mode }),
+          },
+        ],
+        exclude: path.resolve(__dirname, "node_modules"),
+      },
+      {
+        test: /\.(png|jpe?g|gif|webp)(\?.*)?$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 4096,
+              name: "img/[name].[hash:8].[ext]",
+            },
+          },
+        ],
+        exclude: path.resolve(__dirname, "node_modules"),
+      },
+      {
+        test: /\.(svg)(\?.*)?$/,
+        loader: "file-loader",
+        options: {
+          name: "img/[name].[hash:8].[ext]",
+        },
+        exclude: path.resolve(__dirname, "node_modules"),
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 10000,
+              name: "fonts/[name].[hash:7].[ext]",
+            },
+          },
+        ],
+        include: path.resolve(__dirname, "src/fonts"),
+      },
+      {
+        enforce: "pre",
+        test: /\.(vue|js)$/,
+        exclude: [/node_modules/],
+        use: [
+          {
+            loader: "eslint-loader",
+            options: {
+              extensions: [".js", ".vue"],
+              emitWarning: true,
+              formatter: "stylish",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new webpack.DefinePlugin({
+      BASE_URL: '"/"',
+      "process.env": {
+        BASE_URL: '"/"',
+      },
+    }),
+    new CaseSensitivePathsPlugin(),
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+    ...(argv.mode === "production"
+      ? [
+          new MiniCssExtractPlugin({
+            filename: "css/[name].[contenthash:8].css",
+            chunkFilename: "css/[name].[contenthash:8].css",
+          }),
+          new OptimizeCssAssetsPlugin({
+            canPrint: false,
+            cssProcessorOptions: {
+              safe: true,
+              autoprefixer: {
+                disable: true,
+              },
+              mergeLonghand: false,
+            },
+          }),
+        ]
+      : [
+          new webpack.HotModuleReplacementPlugin(),
+          new webpack.NoEmitOnErrorsPlugin(),
+          new webpack.ProgressPlugin(),
+        ]),
+    new HtmlWebpackPlugin({
+      templateParameters: (compilation, assets, pluginOptions) => {
+        // enhance html-webpack-plugin's built in template params
+        let stats;
+        return {
+          // make stats lazy as it is expensive
+          get webpack() {
+            return stats || (stats = compilation.getStats().toJson());
+          },
+          compilation: compilation,
+          webpackConfig: compilation.options,
+          htmlWebpackPlugin: {
+            files: assets,
+            options: pluginOptions,
+          },
+        };
+      },
+      ...(argv.mode === "production"
+        ? {
+            minify: {
+              removeComments: true,
+              collapseWhitespace: true,
+              removeAttributeQuotes: true,
+              collapseBooleanAttributes: true,
+              removeScriptTypeAttributes: true,
+            },
+          }
+        : {}),
+      template: path.resolve(__dirname, "public/index.html"),
+    }),
+    new HtmlPwaPlugin({
+      name: "Minerva Archer",
+      short_name: "Minerva",
+      themeColor: "#000000",
+      msTileColor: "#FFFFFF",
+      manifestOptions: {
+        name: "Minerva Archer",
+        short_name: "Minerva",
+        description: "The personal website of Minerva Archer.",
+        developer: {
+          name: "Minerva Archer",
+          url: "https://minervaarcher.com",
+        },
+        start_url: "/index.html",
+        theme_color: "#000000",
+        background_color: "#FFFFFF",
+        icons: [
+          {
+            src: '/img/icons/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/img/icons/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/img/icons/apple-touch-icon-60x60.png',
+            sizes: '60x60',
+            type: 'image/png',
+          },
+          {
+            src: '/img/icons/apple-touch-icon-76x76.png',
+            sizes: '76x76',
+            type: 'image/png',
+          },
+          {
+            src: '/img/icons/apple-touch-icon.png',
+            sizes: '180x180',
+            type: 'image/png',
+          },
+          {
+            src: '/img/icons/apple-touch-icon-120x120.png',
+            sizes: '120x120',
+            type: 'image/png',
+          },
+          {
+            src: '/img/icons/apple-touch-icon-152x152.png',
+            sizes: '152x152',
+            type: 'image/png',
+          },
+          {
+            src: '/img/icons/apple-touch-icon-180x180.png',
+            sizes: '180x180',
+            type: 'image/png',
+          },
+          {
+            src: '/img/icons/favicon-16x16.png',
+            sizes: '16x16',
+            type: 'image/png',
+          },
+          {
+            src: '/img/icons/favicon-32x32.png',
+            sizes: '32x32',
+            type: 'image/png',
+          },
+          {
+            src: '/img/icons/msapplication-icon-144x144.png',
+            sizes: '144x144',
+            type: 'image/png',
+          },
+          {
+            src: '/img/icons/mstile-150x150.png',
+            sizes: '150x150',
+            type: 'image/png',
+          },
+          {
+            src: '/img/icons/safari-pinned-tab.svg',
+            type: 'image/svg',
+          },
+        ],
+      },
+    }),
+    new PreloadPlugin({
+      rel: "preload",
+      include: "initial",
+      fileBlacklist: [/\.map$/, /hot-update\.js$/],
+    }),
+    new PreloadPlugin({
+      rel: "prefetch",
+      include: "asyncChunks",
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "public"),
+          to: path.resolve(__dirname, "dist"),
+          toType: "dir",
+          globOptions: {
+            ignore: ["**/index.html", "**/.DS_Store"],
+          },
+        },
+      ],
+    }),
+    ...(argv.mode === "production"
+      ? [
+          new GenerateSW({
+            cleanupOutdatedCaches: true,
+            exclude: [
+              /\.map$/,
+              /img\/icons\//,
+              /favicon\.ico$/,
+              /manifest\.json$/,
+            ],
+            cacheId: "minervaarcher-vue",
+          }),
+        ]
+      : []),
+  ],
 });
